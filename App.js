7d7c99bdfc60fe1b4e9be2e17193b58e9a0ff21c
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-02 15:52:45 +0900

    apply hot reloading.

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { createApp, withPhenomicApi } from "@phenomic/preset-react-app/lib/client";
+import { createApp, renderApp } from "@phenomic/preset-react-app/lib/client";
 import { query } from "@phenomic/plugin-renderer-react/lib/client";
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
 import BlogPostContainer from "./components/BlogPostContainer";
 import HomeContainer from "./components/HomeContainer";
 
-export default createApp(() => (
+const routes = () => (
   <Router history={ browserHistory }>
     <Route path="/" component={ HomeContainer } />
     <Route path="/after/:after" component={ HomeContainer } />
     <Route path="/blog/*" component={ BlogPostContainer } />
   </Router>
-));
+);
+
+export default createApp(routes);
+if (module.hot) {
+  module.hot.accept(() => renderApp(routes));
+}
