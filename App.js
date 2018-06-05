311156d5635ca5569dbd3b8f8b4c5fcbbacbb97a
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-06-05 20:52:21 +0900

    support PageError.

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { createApp, renderApp } from "@phenomic/preset-react-app/lib/client";
-import { query } from "@phenomic/plugin-renderer-react/lib/client";
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
 import BlogPostContainer from "./components/BlogPostContainer";
 import HomeContainer from "./components/HomeContainer";
+import PageError from "./components/PageError";
 
 const routes = () => (
   <Router history={ browserHistory }>
     <Route path="/" component={ HomeContainer } />
     <Route path="/after/:after" component={ HomeContainer } />
     <Route path="/blog/*" component={ BlogPostContainer } />
+    <Route path="*" component={ PageError } />
   </Router>
 );
 
--- components/BlogPostContainer.js
+++ components/BlogPostContainer.js
@@ -2,26 +2,32 @@ import { BodyRenderer, createApp, query, withPhenomicApi } from "@phenomic/prese
 import React from "react";
 import { Link } from "react-router";
 import Layout from "./Layout";
+import PageError from "./PageError";
 
-const component = ({ isLoading, page }) => (
-  <Layout>
-    { isLoading && "Loading..." }
-    { !isLoading &&
-      page.node && (
-        <article>
-          <Head>
-            <title>{ page.node.title } - Blog</title>
-            <meta name="description" content="Blog" />
-          </Head>
-          <h1>{ page.node.title }</h1>
-          <BodyRenderer>{ page.node.body }</BodyRenderer>
-        </article>
-      )}
-    <footer>
-      <Link to="/">Go to home</Link>
-    </footer>
-  </Layout>
-);
+const component = ({ hasError, isLoading, page }) => {
+  if (hasError) {
+    return <PageError error={ page.error } />;
+  }
+  return (
+    <Layout>
+      { isLoading && "Loading..." }
+      { !isLoading &&
+        page.node && (
+          <article>
+            <Head>
+              <title>{ page.node.title } - Blog</title>
+              <meta name="description" content="Blog" />
+            </Head>
+            <h1>{ page.node.title }</h1>
+            <BodyRenderer>{ page.node.body }</BodyRenderer>
+          </article>
+        )}
+      <footer>
+        <Link to="/">Go to home</Link>
+      </footer>
+    </Layout>
+  )
+};
 
 export default withPhenomicApi(component, props => ({
   page: query({ path: "content/posts", id: props.params.splat }),
